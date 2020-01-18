991d6056acee29d2973f95e93a928879967a7ff5
aisva <aisasi@gmail.com>
2020-01-18 14:02:29 +0100

    Set up  API endpoint in React client

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,5 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-//export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
-export const apiEndpoint = "http://localhost:3003"
+const apiId = 'bhhftqamo2'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'dev-qfv-c16y.auth0.com',
